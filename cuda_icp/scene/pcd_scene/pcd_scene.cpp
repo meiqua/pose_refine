1b5c469d13bf3052e3d47227767d398c40bce7cb
meiqua <563714976@qq.com>
2019-03-14 21:14:47 +0800

    add a tip

--- cuda_icp/scene/pcd_scene/pcd_scene.cpp
+++ cuda_icp/scene/pcd_scene/pcd_scene.cpp
@@ -41,6 +41,7 @@ void Scene_nn::init_Scene_nn_cpu(cv::Mat &scene_depth__, Mat3x3f &scene_K, KDTre
 // make it easy to transform to cuda implementation
 // the remained hard part is that idx of nodes are dependent
 // use cuda atomicAdd(fetch and add) looks fine
+// not enough, may need Dynamic Parallelism in cuda
 void KDTree_cpu::build_tree(int max_num_pcd_in_leaf)
 {
     assert(pcd_buffer.size() > 0 && pcd_buffer.size() == normal_buffer.size()
