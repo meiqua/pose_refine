dfd80efbebe10feafa42b570be9214d80cc5a483
meiqua <563714976@qq.com>
2019-04-26 11:40:18 +0800

    fix rasterization

--- cuda_renderer/renderer.cu
+++ cuda_renderer/renderer.cu
@@ -99,9 +99,9 @@ void rasterization(const Model::Triangle dev_tri, Model::float3 last_row,
 
     size_t real_width = width;
     if(roi.width > 0 && roi.height > 0){  // depth will be flipped
-        clamp_min[0] = width - (roi.x + roi.width) + 1; // +1 avoid out of roi
+        clamp_min[0] = width-1 - (roi.x + roi.width) + 1; // +1 avoid out of roi
         clamp_min[1] = height-1 - (roi.y + roi.height) + 1;
-        clamp_max[0] = width - roi.x;
+        clamp_max[0] = width-1 - roi.x;
         clamp_max[1] = height-1 - roi.y;
         real_width = roi.width;
     }
@@ -128,7 +128,7 @@ void rasterization(const Model::Triangle dev_tri, Model::float3 last_row,
             float frag_depth = -(dev_tri.v0.z*bc_over_z.x + dev_tri.v1.z*bc_over_z.y + dev_tri.v2.z*bc_over_z.z)
                     /(bc_over_z.x + bc_over_z.y + bc_over_z.z);
 
-            size_t x_to_write = (width - P[0] - roi.x);
+            size_t x_to_write = (width-1 - P[0] - roi.x);
             size_t y_to_write = (height-1 - P[1] - roi.y);
 
             int32_t depth = int32_t(frag_depth/**1000*/ + 0.5f);
