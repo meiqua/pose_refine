7a71e19dc48473b0164c3530174ce491ac56c73b
meiqua <563714976@qq.com>
2020-03-20 23:07:34 +0800

    switch to cuda build tree

--- cuda_icp/scene/pcd_scene/pcd_scene.cu
+++ cuda_icp/scene/pcd_scene/pcd_scene.cu
@@ -11,8 +11,9 @@ void Scene_nn::init_Scene_nn_cuda(cv::Mat &scene_depth, Mat3x3f &scene_K, KDTree
     kdtree.normal_buffer.__malloc(cpu_tree.normal_buffer.size());
     thrust::copy(cpu_tree.normal_buffer.begin(), cpu_tree.normal_buffer.end(), kdtree.normal_buffer.begin_thr());
 
-    kdtree.nodes.__malloc(cpu_tree.nodes.size());
-    thrust::copy(cpu_tree.nodes.begin(), cpu_tree.nodes.end(), kdtree.nodes.begin_thr());
+    // kdtree.nodes.__malloc(cpu_tree.nodes.size());
+    // thrust::copy(cpu_tree.nodes.begin(), cpu_tree.nodes.end(), kdtree.nodes.begin_thr());
+    kdtree.build_tree();
 
     pcd_ptr = kdtree.pcd_buffer.data();
     normal_ptr = kdtree.normal_buffer.data();
